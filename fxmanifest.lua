ccb409dda5275eda4841fec31e7205169c2f2fd3
TazElite <adamkobers24@gmail.com>
2026-09-25 05:41:14 +0400

    Improve FiveM resource metadata

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'KruigerLabs'
-description 'Coordinate copier for FiveM'
+author 'Kruiger Labs LLC'
+description 'Free standalone FiveM coordinates script for copying vector4 coordinates and heading.'
 version '1.0.0'
 
 ui_page 'html/index.html'
